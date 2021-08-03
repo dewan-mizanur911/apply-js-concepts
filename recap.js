59f733aa3168c72a44395bccdd7b4b51b2d057cf
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-08-03 22:18:01 +0600

    initialized with recap

---
+++ recap.js
@@ -0,0 +1,25 @@
+// variables 
+var time = '10:38pm';
+var marks = 88;
+var isColorWhite = false;
+
+// push, pop, shift, unshift, concat
+
+// conditionals
+if(marks < 90){
+    console.log();
+}
+else{}
+
+// let const
+
+// value of the variable could change
+let price = 27;
+price = 29;
+price = 31;
+
+// value of the variable will not change
+const name = 'Dewan vai';
+
+// loop
+// function
